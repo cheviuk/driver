a2285c894b078b375c821900309d6034dc955dd2
daria <daria>
2017-02-22 01:26:49 +0200

    Initial commit

---
+++ src/main/java/ua/od/driver/Driver.java
@@ -0,0 +1,17 @@
+package ua.od.driver;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class Driver {
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+        return Response.status(200).entity(output).build();
+    }
+}
